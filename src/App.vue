970e6c6ef0f00fe787cbba2375d41a99e3fa3e05
amanggupta <amanguptaymail@gmail.com>
2021-03-13 12:22:13 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app dark color="blue">
+     <v-toolbar-title>Vue login</v-toolbar-title>
+     <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+    <v-btn text rounded>Login</v-btn>
+    <v-btn text rounded>Contact</v-btn>
+    <v-btn text rounded>About us</v-btn>  
+    </v-app-bar>
+
+ <v-content>
+   <v-card width="500" class="mx-auto mt-9 ">
+     <v-card-title>Login area </v-card-title>
+     <v-card-text>
+       <v-text-field label="Username" prepend-icon="mdi-account-circle"/>
+       <v-text-field label="Password" :type="showPassword ?'text' : 'password'"
+       prepend-icon="mdi-lock" :append-icon="showPassword?'mdi-eye' :'mdi-eye-off'"
+       @click:append="showPassword =!showPassword "/>
+     </v-card-text>
+     <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+   </v-card>
+ </v-content>
+
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+showPassword:false
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
